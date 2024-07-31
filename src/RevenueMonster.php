a81e6d7b2e81d7b003eda18b092579c3da23aa2e
AnasAdham <64723624+AnasAdham@users.noreply.github.com>
2024-07-31 15:12:27 +0800

    Update RevenueMonster.php

--- src/RevenueMonster.php
+++ src/RevenueMonster.php
@@ -44,6 +44,7 @@ class RevenueMonster
         'user', Modules\UserModule::class,
         'payment' => Modules\PaymentModule::class,
         'ekyc' => Modules\EkycModule::class,
+        // Some changes
     ];
 
     public function __construct(array $arguments = [])
